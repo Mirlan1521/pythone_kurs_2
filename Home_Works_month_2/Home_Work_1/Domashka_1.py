d0e31562c2d6ec1dd8e68d1f10910fd1f58aeef4
@Kuruchbekovich <mirlan_33-3@mail.ru>
2021-05-06 10:43:11 +0600

    DZ#1

--- Home_Works_month_2/Home_Work_1/Domashka_1.py
+++ Home_Works_month_2/Home_Work_1/Domashka_1.py
@@ -1,1 +1,79 @@
-print("Hello github")
+# Пример add
+class Fraction:
+
+    def __init__(self, numenator, denumenator):
+
+        self.num = numenator
+        self.den = denumenator
+
+    def __str__(self):
+        return str(self.num) + "/" + str(self.den)
+
+    def __add__(self, otherfraction):
+        if self.den == otherfraction.den:
+            newden = self.den
+            newnum = self.num + otherfraction.num
+        else:
+            newnum = self.num * otherfraction.den + self.den * otherfraction.num
+            newden = self.den * otherfraction.den
+
+        return Fraction(newnum, newden)
+
+
+f1 = Fraction(10, 5)
+
+f2 = Fraction(7, 5)
+
+print(f1 + f2)
+
+#Пример с Multi
+class Fraction:
+
+    def __init__(self, numenator, denumenator):
+        self.num = numenator
+        self.den = denumenator
+
+    def __str__(self):
+        return str(self.num) + "/" + str(self.den)
+
+    def __mul__(self, otherfraction):
+        newnum = self.num * otherfraction.num
+        newden = self.den * otherfraction.den
+
+        return Fraction(newnum, newden)
+
+
+f1 = Fraction(10, 5)
+
+f2 = Fraction(7, 5)
+
+print(f1 * f2)
+
+# Пример с subtract
+class Fraction:
+
+    def __init__(self, numenator, denumenator):
+
+        self.num = numenator
+        self.den = denumenator
+
+    def __str__(self):
+        return str(self.num) + "/" + str(self.den)
+
+    def __sub__(self, otherfraction):
+        if self.den == otherfraction.den:
+            newden = self.den
+            newnum = self.num - otherfraction.num
+        else:
+            newnum = self.num * otherfraction.den - self.den * otherfraction.num
+            newden = self.den * otherfraction.den
+
+        return Fraction(newnum, newden)
+
+
+f1 = Fraction(10, 6)
+
+f2 = Fraction(7, 5)
+
+print(f1 - f2)
+
