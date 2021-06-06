ffdc1108ea9b298973b70b2850c3e99eae4d965e
@Kuruchbekovich <mirlan_33-3@mail.ru>
2021-06-07 03:00:09 +0600

    Создал игру

--- Home_Works_month_2/Home_Work_4/DZ#4.py
+++ Home_Works_month_2/Home_Work_4/DZ#4.py
@@ -1,1 +1,16 @@
+from random import randrange
 
+number = randrange(10)
+
+vvod = -1
+kolichestvo = 0
+
+while vvod != number:
+    vvod = int(input("Введите ваше число   "))
+    kolichestvo += 1
+    if vvod > number:
+        print("введено число больше попытайся еще раз")
+    elif vvod < number:
+        print("введено число меньше попытайся еще раз")
+
+print("Ураа ты угадал число")
